ff106430a5a1b3702ded09780da58511ba12b36e
jakpas3 <jakpas3@st.amu.edu.pl>
2024-10-06 16:37:57 +0200

    Upload a local file
    to a defined location in the bucket

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 AWS_BUCKET_NAME = os.getenv('AWS_BUCKET_NAME')
 AWS_BUCKET_PREFIX = os.getenv('AWS_BUCKET_PREFIX')
 
-print(ACCESS_KEY)
-
 s3 = boto3.client(
     's3',
     aws_access_key_id=ACCESS_KEY,
@@ -22,4 +20,10 @@ def list_files(bucket_name, prefix):
     response = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
     return [item['Key'] for item in response.get('Contents', [])]
 
+def upload_file(file_name, bucket_name, prefix, object_name):
+    object_name = prefix + file_name
+    response = s3.upload_file(file_name, bucket_name, object_name)
+
+print(list_files(AWS_BUCKET_NAME, AWS_BUCKET_PREFIX))
+upload_file('test.txt', AWS_BUCKET_NAME, AWS_BUCKET_PREFIX, None)
 print(list_files(AWS_BUCKET_NAME, AWS_BUCKET_PREFIX))
